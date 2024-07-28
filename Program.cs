5e02ca1be2d3e2028c8f530dd59f0e1ba271e12d
Chris Mehn <67209636+mehnchris@users.noreply.github.com>
2024-07-28 04:20:03 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using MailKit;
+using SendMail.Interfaces;
 using SendMail.Models;
 using SendMail.Services;
 
@@ -6,15 +7,12 @@ using SendMail.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
-builder.Services.AddTransient<IMailSendService, MailSendService>();
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+builder.Services.AddTransient<IMailSendServices, MailSendService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
